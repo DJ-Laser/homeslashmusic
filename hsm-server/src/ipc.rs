ee88613bc6a721932481d6ea5ac11ea691a5c6fc
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-07-19 22:18:08 -0400

    make streams run in parallell

--- hsm-server/src/ipc.rs
+++ hsm-server/src/ipc.rs
@@ -1,42 +1,66 @@
-use std::{convert::Infallible, fs, path::PathBuf};
+use std::{fs, path::PathBuf, sync::Arc};
 
 use hsm_ipc::{
   Reply, requests, responses,
   server::{RequestHandler, handle_request},
 };
 use smol::{
+  Executor,
   io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader},
   net::unix::{UnixListener, UnixStream},
   stream::StreamExt,
 };
 
-pub struct IpcServer {
+pub struct IpcServer<'ex> {
   socket_path: PathBuf,
+  ex: Arc<Executor<'ex>>,
 }
 
-impl IpcServer {
-  pub fn new() -> Self {
+impl<'ex> IpcServer<'ex> {
+  pub fn new(ex: Arc<Executor<'ex>>) -> Self {
     Self {
       socket_path: PathBuf::from(hsm_ipc::socket_path()),
+      ex,
     }
   }
 
-  pub async fn run(&mut self) -> Result<Infallible, io::Error> {
+  pub async fn run(&mut self) -> Result<(), io::Error>
+where {
     let listener = UnixListener::bind(&self.socket_path)?;
 
     while let Some(stream) = listener.incoming().next().await {
-      let res = if let Ok(stream) = stream {
-        self.handle_stream(stream).await
-      } else {
-        stream.map(|_| ())
-      };
+      self
+        .ex
+        .spawn(async move {
+          let res = if let Ok(stream) = stream {
+            StreamHandler::new().handle_stream(stream).await
+          } else {
+            stream.map(|_| ())
+          };
 
-      if let Err(error) = res {
-        eprintln!("failed to connect to ipc client: {}", error);
-      }
+          if let Err(error) = res {
+            eprintln!("failed to connect to ipc client: {}", error);
+          }
+        })
+        .detach();
     }
 
     unreachable!("Iterating over `Incoming` should never return `None`")
+  }
+}
+
+impl<'ex> Drop for IpcServer<'ex> {
+  fn drop(&mut self) {
+    let _ = fs::remove_file(&self.socket_path);
+    println!("Removing socket: `{:?}`", self.socket_path)
+  }
+}
+
+struct StreamHandler {}
+
+impl StreamHandler {
+  fn new() -> Self {
+    Self {}
   }
 
   async fn handle_stream(&self, stream: UnixStream) -> io::Result<()> {
@@ -53,15 +77,8 @@ impl IpcServer {
   }
 }
 
-impl RequestHandler for IpcServer {
+impl RequestHandler for StreamHandler {
   fn handle_version(&self, _: requests::Version) -> Reply<requests::Version> {
     Ok(responses::Version(hsm_ipc::version()))
-  }
-}
-
-impl Drop for IpcServer {
-  fn drop(&mut self) {
-    fs::remove_file(&self.socket_path);
-    println!("Removing socket: `{:?}`", self.socket_path)
   }
 }
--- hsm-server/src/main.rs
+++ hsm-server/src/main.rs
@@ -1,19 +1,20 @@
+use std::sync::{Arc, OnceLock};
+
 use ctrlc::CtrlCHandler;
 use ipc::IpcServer;
+use smol::Executor;
 
 mod ctrlc;
 mod ipc;
 
 fn main() {
-  let ex = smol::Executor::new();
+  static EX: OnceLock<Arc<Executor>> = OnceLock::new();
+  EX.get_or_init(|| Arc::new(Executor::new()));
+
+  let ex: &'static Arc<Executor<'static>> = EX.get().unwrap();
   let ctrlc = CtrlCHandler::init();
 
-  let stream_handle =
-    rodio::OutputStreamBuilder::open_default_stream().expect("Could not open default audio stream");
-
-  let channel = smol::channel::unbounded::<usize>();
-
-  let ipc_server = ex.spawn(async { IpcServer::new().run().await.unwrap() });
+  let ipc_server = ex.spawn(async { IpcServer::new(ex.clone()).run().await.unwrap() });
 
   smol::block_on(ex.run(async {
     ctrlc.wait_for_ctrlc().await;
