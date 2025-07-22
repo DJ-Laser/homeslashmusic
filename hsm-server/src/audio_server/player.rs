cd877e38ad622e77321350df04f897348fa0f5b6
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-07-21 20:48:43 -0400

    add controls to player

--- hsm-server/src/audio_server/player.rs
+++ hsm-server/src/audio_server/player.rs
@@ -2,20 +2,52 @@ use std::{
   fs::File as SyncFile,
   io::{self, BufReader as SyncBufReader},
   path::Path,
-  sync::Arc,
+  sync::{
+    Arc,
+    atomic::{AtomicUsize, Ordering},
+  },
+  time::Duration,
 };
 
+use atomic_enum::atomic_enum;
 use rodio::{
   Decoder, Source,
   mixer::Mixer,
   queue::{SourcesQueueInput, SourcesQueueOutput, queue},
 };
+use smol::lock::Mutex;
+
+#[atomic_enum]
+enum PlaybackState {
+  Playing,
+  Paused,
+  Stopped,
+}
+
+struct Controls {
+  pub playback_state: AtomicPlaybackState,
+  pub to_skip: AtomicUsize,
+  pub volume: Mutex<f32>,
+}
+
+impl Controls {
+  pub fn new() -> Self {
+    Self {
+      playback_state: AtomicPlaybackState::new(PlaybackState::Stopped),
+      to_skip: AtomicUsize::new(0),
+      volume: Mutex::new(1.0),
+    }
+  }
+}
 
 pub struct Player {
   track_queue: Arc<SourcesQueueInput>,
+  controls: Arc<Controls>,
 }
 
 impl Player {
+  const SOURCE_UPDATE_INTERVAL: Duration = Duration::from_millis(5);
+
   pub fn connect_new(mixer: &Mixer) -> Self {
     let (player, source) = Self::new();
     mixer.add(source);
@@ -28,14 +60,39 @@ impl Player {
     (
       Self {
         track_queue: queue_in,
+        controls: Arc::new(Controls::new()),
       },
       queue_out,
     )
   }
 
-  async fn load_file<P: AsRef<Path>>(
-    path: P,
-  ) -> Result<Decoder<SyncBufReader<SyncFile>>, io::Error> {
+  fn wrap_source<S: Source + Send + 'static>(&self, source: S) -> impl Source + 'static {
+    let controls = self.controls.clone();
+    source
+      .track_position()
+      .amplify(1.0)
+      .pausable(false)
+      .skippable()
+      .periodic_access(Self::SOURCE_UPDATE_INTERVAL, move |skippable| {
+        {
+          let to_skip = controls.to_skip.load(Ordering::Acquire);
+          if to_skip > 0 {
+            skippable.skip();
+            controls.to_skip.store(to_skip - 1, Ordering::Release);
+            return;
+          }
+        }
+        let pauseable = skippable.inner_mut();
+        pauseable.set_paused(matches!(
+          controls.playback_state.load(Ordering::Relaxed),
+          PlaybackState::Paused
+        ));
+        let volume_controlled = pauseable.inner_mut();
+        volume_controlled.set_factor(*controls.volume.lock_blocking());
+      })
+  }
+
+  async fn load_file<P: AsRef<Path>>(&self, path: P) -> Result<impl Source + 'static, io::Error> {
     let path = path.as_ref().to_owned();
     let (path, file, len) = smol::unblock(|| {
       let file = SyncFile::open(&path)?;
@@ -53,12 +110,16 @@ impl Player {
       builder = builder.with_hint(extension);
     }
 
-    Ok(builder.build().map_err(io::Error::other)?)
+    let decoder = builder.build().map_err(io::Error::other)?;
+    Ok(self.wrap_source(decoder))
   }
 
   pub async fn run(&mut self) {
     loop {
-      let source = Self::load_file("").await.unwrap();
+      let source = self
+        .load_file("/home/dj_laser/Music/ASGORE - trap remix w_ mythic apex.mp3")
+        .await
+        .unwrap();
       let duration = source.total_duration().unwrap();
       self.track_queue.append(source);
       smol::Timer::after(duration).await;
