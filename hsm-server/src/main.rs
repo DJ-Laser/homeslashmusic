0dfbf724987a01765c47836c1a9ceb305025092a
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-09-05 21:36:24 -0400

    handle errors properly instead of unwrapping

--- hsm-server/src/main.rs
+++ hsm-server/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{error::Error, sync::Arc};
 
 use audio_server::AudioServer;
 use futures_concurrency::future::Race;
@@ -12,28 +12,36 @@ mod ipc;
 mod mpris_server;
 mod signals;
 
+async fn run_servers(ex: &Arc<Executor<'static>>) -> Result<(), Box<dyn Error>> {
+  let mut signal_handler = SignalHandler::init()?;
+
+  let (audio_server, message_tx) = AudioServer::init();
+  let ipc_server = IpcServer::new(message_tx.clone(), ex.clone())?;
+  let mpris_server = MprisServer::init(
+    message_tx.clone(),
+    audio_server.register_event_listener().await,
+  )
+  .await?;
+
+  let server_futures = (
+    async move { ipc_server.run().await.map_err(Into::<Box<dyn Error>>::into) },
+    async { audio_server.run().await.map_err(Into::into) },
+    async { mpris_server.run().await.map_err(Into::into) },
+    async {
+      signal_handler.wait_for_quit().await;
+      Ok(())
+    },
+  );
+
+  server_futures.race().await
+}
+
 fn main() {
   let ex: Arc<Executor<'static>> = Arc::new(Executor::new());
+  match smol::block_on(ex.run(run_servers(&ex))) {
+    Ok(()) => (),
+    Err(error) => eprintln!("{error}"),
+  }
 
-  smol::block_on(ex.run(async {
-    let mut signal_handler = SignalHandler::init().unwrap();
-
-    let (audio_server, message_tx) = AudioServer::init();
-    let mpris_server = MprisServer::init(
-      message_tx.clone(),
-      audio_server.register_event_listener().await,
-    )
-    .await
-    .unwrap();
-    let ipc_server = IpcServer::new(message_tx.clone(), ex.clone()).unwrap();
-
-    (
-      async move { ipc_server.run().await.unwrap() },
-      async { audio_server.run().await.unwrap() },
-      async { mpris_server.run().await.unwrap() },
-      signal_handler.wait_for_quit(),
-    )
-      .race()
-      .await;
-  }));
+  println!("hsm-server shutting down");
 }
